89a8588b8837483654942535c0bb4a0d27ddcd0b
jaydewhyte1992 <135990890+jaydewhyte1992@users.noreply.github.com>
2024-02-19 17:40:43 +1100

    working on the weather details

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = "Monday 17:34";
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  speedElement.innerHTML = `${response.data.wind.speed}km/hr`;
 }
 
 function searchCity(city) {
