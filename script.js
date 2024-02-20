23e0cbeb6b1598ab52cf6287d400f2f2eddb0ff8
jaydewhyte1992 <135990890+jaydewhyte1992@users.noreply.github.com>
2024-02-20 13:47:45 +1100

    inject the forecast HTML from Javascript

--- script.js
+++ script.js
@@ -53,7 +53,31 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = [`Tue`, `Wed`, `Thu`, `Fri`, `Sat`];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature">
+        <strong>15°</strong>
+      </span>
+      <span class="weather-forecast-temperature">9°</span>
+    </div>
+  </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Melbourne");
+displayForecast();
